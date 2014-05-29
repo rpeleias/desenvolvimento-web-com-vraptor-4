1ed48b6a026495b4a077446083de361dd3b80a4a
Rodrigo Turini <rdrg.turini@gmail.com>
2014-05-29 18:46:16 -0300

    codigo do quarto capitulo

--- src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
+++ src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.dao.ProdutoDao;
 import br.com.caelum.vraptor.model.Produto;
 import br.com.caelum.vraptor.util.JPAUtil;
@@ -17,18 +19,18 @@ public class ProdutoController {
     public void inicio() {
     }
 	
-    @Path("/produto/lista")
+    @Get
     public List<Produto> lista() {
         EntityManager em = JPAUtil.criaEntityManager();
         ProdutoDao dao = new ProdutoDao(em);
         return dao.lista();
     }
 
-    @Path("/produto/formulario")
+    @Get
     public void formulario(){
     }
     
-    @Path("/produto/adiciona")
+    @Post
     public void adiciona(Produto produto) {
         EntityManager em = JPAUtil.criaEntityManager();
         em.getTransaction().begin();
