bb122afbf871e625f69833f050474908c288ca2c
Rodrigo Turini <rdrg.turini@gmail.com>
2014-05-30 17:57:27 -0300

    codigo do sexto capitulo

--- src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
+++ src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -1,7 +1,6 @@
 package br.com.caelum.vraptor.controller;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
@@ -10,21 +9,22 @@ import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.dao.ProdutoDao;
 import br.com.caelum.vraptor.model.Produto;
-import br.com.caelum.vraptor.util.JPAUtil;
 import br.com.caelum.vraptor.view.Results;
 
 @Controller
 public class ProdutoController {
 	
 	private final Result result;
+	private ProdutoDao dao;
 
 	@Inject
-	public ProdutoController(Result result) {
+	public ProdutoController(Result result, ProdutoDao dao) {
 	    this.result = result;
+		this.dao = dao;
 	}
 	
 	protected ProdutoController() {
-		this(null);
+		this(null, null);
 	}
 	
 	@Path("/")
@@ -33,22 +33,16 @@ public class ProdutoController {
 	
 	@Get
 	public void lista() {
-	    EntityManager em = JPAUtil.criaEntityManager();
-	    ProdutoDao dao = new ProdutoDao(em);
 	    result.include("produtoList", dao.lista());
 	}
 	
 	@Get
 	public void listaEmXml() {
-	    EntityManager em = JPAUtil.criaEntityManager();
-	    ProdutoDao dao = new ProdutoDao(em);
 	    result.use(Results.xml()).from(dao.lista()).exclude("quantidade").serialize();
 	}
 	
 	@Get
 	public void listaEmJson() {
-	    EntityManager em = JPAUtil.criaEntityManager();
-	    ProdutoDao dao = new ProdutoDao(em);
 	    result.use(Results.json()).from(dao.lista()).serialize();
 	}
 
@@ -58,11 +52,7 @@ public class ProdutoController {
     
     @Post
     public void adiciona(Produto produto) {
-        EntityManager em = JPAUtil.criaEntityManager();
-        em.getTransaction().begin();
-        ProdutoDao dao = new ProdutoDao(em);
         dao.adiciona(produto);
-        em.getTransaction().commit();
         result.include("mensagem", "Produto adicionado com sucesso!");
         result.redirectTo(this).lista();
     }
--- src/main/java/br/com/caelum/vraptor/dao/ProdutoDao.java
+++ src/main/java/br/com/caelum/vraptor/dao/ProdutoDao.java
@@ -2,24 +2,36 @@ package br.com.caelum.vraptor.dao;
 
 import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import br.com.caelum.vraptor.model.Produto;
 
+@RequestScoped
 public class ProdutoDao {
 
 	private final EntityManager em;
 
+	@Inject
 	public ProdutoDao(EntityManager em) {
 		this.em = em;
 	}
 	
+	protected ProdutoDao() {
+		this(null);
+	}
+	
 	public void adiciona(Produto produto) {
+		em.getTransaction().begin();
 		em.persist(produto);
+		em.getTransaction().commit();
 	}
 
 	public void remove(Produto produto) {
+		em.getTransaction().begin();
 		em.remove(busca(produto));
+		em.getTransaction().commit();
 	}
 
 	public Produto busca(Produto produto) {
